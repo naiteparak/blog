1d16b5ac19aa22cc32549161d2b732bcf5f34f30
Ash Karapetyan <ashot.karapetyan.y@gmail.com>
2023-05-12 13:24:21 +0400

    Basic setup

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('The Blog')
+    .setDescription('The Blog API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+    console.log(`App docs on http://localhost:${port}/api`);
+  });
+}
+bootstrap();
